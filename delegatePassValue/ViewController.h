c8f7355f6df0aba5ecf2cb78af3e8bda5f955021
chendi <bbigcd@163.com>
2016-08-09 08:58:24 +0800

    Initial Commit

---
+++ delegatePassValue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  delegatePassValue
+//
+//  Created by bbigcd on 16/8/9.
+//  Copyright © 2016年 cd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ delegatePassValue/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  delegatePassValue
+//
+//  Created by bbigcd on 16/8/9.
+//  Copyright © 2016年 cd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
